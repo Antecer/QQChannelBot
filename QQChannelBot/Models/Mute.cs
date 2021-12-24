e6adb492c01748efc58c4bfb3c300002bd0aafa1
Antecer <antecer@outlook.com>
2021-12-24 15:17:25 +0800

    增强禁言API功能，提高调用自由度。

--- QQChannelBot/Models/Mute.cs
+++ QQChannelBot/Models/Mute.cs
@@ -1,4 +1,5 @@
 ﻿using System.Text.Json.Serialization;
+using System.Text.RegularExpressions;
 
 namespace QQChannelBot.Models
 {
@@ -7,6 +8,10 @@ namespace QQChannelBot.Models
     /// </summary>
     public class MuteTime
     {
+        public MuteTime()
+        {
+            MuteSeconds = "60";
+        }
         /// <summary>
         /// 禁言指定的时长
         /// </summary>
@@ -38,5 +43,53 @@ namespace QQChannelBot.Models
         /// </summary>
         [JsonPropertyName("mute_seconds")]
         public string? MuteSeconds { get; set; }
+    }
+
+    /// <summary>
+    /// 根据时间字符串构建禁言时间
+    /// </summary>
+    public class MuteMake : MuteTime
+    {
+        readonly Regex TypeTimeStamp = new(@"(\d{4})[-年](\d\d)[-月](\d\d)[\s日]*(\d\d)[:点时](\d\d)[:分](\d\d)秒?");
+        readonly Regex TypeTimeDelay = new(@"(\d+)\s*(年|星期|周|日|天|小?时|分钟?|秒钟?)");
+        /// <summary>
+        /// 构造禁言时间
+        /// <para>
+        /// 支持以下正则匹配的格式 (优先使用时间戳模式)：<br/>
+        /// 时间戳模式 - "^(\d{4})[-年](\d\d)[-月](\d\d)[\s日]*(\d\d)[:点时](\d\d)[:分](\d\d)秒?\s*$"<br/>
+        /// 倒计时模式 - "^(\d+)\s*(年|星期|周|日|天|小?时|分钟?|秒钟?)?\s*$"
+        /// </para>
+        /// </summary>
+        public MuteMake(string timeString = "1分钟")
+        {
+            if (string.IsNullOrEmpty(timeString)) return;
+            Match mts = TypeTimeStamp.Match(timeString);
+            if (mts.Success)
+            {
+                string timstamp = $"{mts.Groups[1].Value}-{mts.Groups[2].Value}-{mts.Groups[3].Value} {mts.Groups[4].Value}:{mts.Groups[5].Value}:{mts.Groups[6].Value}";
+                MuteEndTimestamp = new DateTimeOffset(Convert.ToDateTime(timstamp)).ToUnixTimeSeconds().ToString();
+            }
+            else
+            {
+                Match mtd = TypeTimeDelay.Match(timeString);
+                if (mtd.Success)
+                {
+                    int seconds = mtd.Groups[2].Value switch
+                    {
+                        "年" => 60 * 60 * 24 * 365,
+                        "星期" => 60 * 60 * 24 * 7,
+                        "周" => 60 * 60 * 24 * 7,
+                        "日" => 60 * 60 * 24,
+                        "天" => 60 * 60 * 24,
+                        "小时" => 60 * 60,
+                        "时" => 60 * 60,
+                        "分钟" => 60,
+                        "分" => 60,
+                        _ => 1
+                    } * int.Parse(mtd.Groups[1].Value);
+                    MuteSeconds = seconds.ToString();
+                }
+            }
+        }
     }
 }
