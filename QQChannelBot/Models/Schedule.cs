d75c59edb14b639674191bf7de921bec750da870
Antecer <antecer@outlook.com>
2022-01-04 00:50:46 +0800

    修改日程对象，添加构造函数和注释

--- QQChannelBot/Models/Schedule.cs
+++ QQChannelBot/Models/Schedule.cs
@@ -1,4 +1,6 @@
-﻿namespace QQChannelBot.Models
+﻿using System.Text.Json.Serialization;
+
+namespace QQChannelBot.Models
 {
     /// <summary>
     /// 日程对象
@@ -6,44 +8,99 @@
     public class Schedule
     {
         /// <summary>
+        /// 新建日程
+        /// </summary>
+        /// <param name="name">日程名称</param>
+        /// <param name="desc">日程描述</param>
+        /// <param name="startTime">开始时间（默认当前时刻）</param>
+        /// <param name="endTime">结束时间（默认持续一天）</param>
+        /// <param name="jumpChannel">日程开始时跳转的频道</param>
+        /// <param name="remindType">日程时间即将到达时的提醒方式</param>
+        public Schedule(
+            string name = "新建日程",
+            string desc = "新的日程",
+            DateTime? startTime = null,
+            DateTime? endTime = null,
+            Channel? jumpChannel = null,
+            ScheduleRemindType remindType = ScheduleRemindType.Never)
+        {
+            Name = name;
+            Description = desc;
+            StartTimestamp = new DateTimeOffset(startTime ?? DateTime.Now).ToUnixTimeMilliseconds().ToString();
+            EndTimestamp = new DateTimeOffset(endTime ?? DateTime.Now.AddDays(1)).ToUnixTimeMilliseconds().ToString();
+            JumpChannelId = jumpChannel?.Id;
+            RemindType = remindType;
+        }
+        /// <summary>
         /// 日程 id
         /// </summary>
-        public string? Id { get; set; }
+        [JsonPropertyName("id")]
+        public string? Id { get; }
         /// <summary>
         /// 日程名称
         /// </summary>
+        [JsonPropertyName("name")]
         public string? Name { get; set; }
         /// <summary>
         /// 日程描述
         /// </summary>
+        [JsonPropertyName("description")]
         public string? Description { get; set; }
         /// <summary>
         /// 日程开始时间戳(ms)
         /// </summary>
+        [JsonPropertyName("start_timestamp")]
         public string? StartTimestamp { get; set; }
         /// <summary>
         /// 日程结束时间戳(ms)
         /// </summary>
+        [JsonPropertyName("end_timestamp")]
         public string? EndTimestamp { get; set; }
         /// <summary>
         /// 创建者
         /// </summary>
-        public Member? Creator { get; set; }
+        [JsonPropertyName("creator")]
+        public Member? Creator { get; }
         /// <summary>
         /// 日程开始时跳转到的子频道 id
         /// </summary>
+        [JsonPropertyName("jump_channel_id")]
         public string? JumpChannelId { get; set; }
         /// <summary>
-        /// 日程提醒类型，取值参考RemindType
-        /// <para>
-        /// 0 - 不提醒 <br/>
-        /// 1 - 开始时提醒 <br/>
-        /// 2 - 开始前5分钟提醒 <br/>
-        /// 3 - 开始前15分钟提醒 <br/>
-        /// 4 - 开始前30分钟提醒 <br/>
-        /// 5 - 开始前60分钟提醒 <br/>
-        /// </para>
+        /// 日程提醒类型
         /// </summary>
-        public string? RemindType { get; set; }
+        [JsonPropertyName("remind_type")]
+        public ScheduleRemindType? RemindType { get; set; }
+    }
+
+    /// <summary>
+    /// 日程提醒方式
+    /// </summary>
+    public enum ScheduleRemindType
+    {
+        /// <summary>
+        /// 不提醒
+        /// </summary>
+        Never,
+        /// <summary>
+        /// 开始时提醒
+        /// </summary>
+        OnStart,
+        /// <summary>
+        /// 开始前5分钟提醒
+        /// </summary>
+        Early5Min,
+        /// <summary>
+        /// 开始前15分钟提醒
+        /// </summary>
+        Early15Min,
+        /// <summary>
+        /// 开始前30分钟提醒
+        /// </summary>
+        Early30Min,
+        /// <summary>
+        /// 开始前60分钟提醒
+        /// </summary>
+        Early60Min
     }
 }
