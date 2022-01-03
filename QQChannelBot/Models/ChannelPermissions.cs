21417c1ef22b7df509d457fc52498f5f5bdda092
Antecer <antecer@outlook.com>
2022-01-03 20:10:22 +0800

    子频道权限添加新属性

--- QQChannelBot/Models/ChannelPermissions.cs
+++ QQChannelBot/Models/ChannelPermissions.cs
@@ -8,15 +8,22 @@ namespace QQChannelBot.Models
     public class ChannelPermissions
     {
         /// <summary>
-        /// 子频道 id
+        /// 子频道Id
         /// </summary>
         [JsonPropertyName("channel_id")]
         public string ChaannelId { get; set; } = "";
         /// <summary>
-        /// 用户 id
+        /// 用户Id
+        /// <para><em>此属性和RoleId只会同时存在一个</em></para>
         /// </summary>
         [JsonPropertyName("user_id")]
-        public string UserId { get; set; } = "";
+        public string? UserId { get; set; }
+        /// <summary>
+        /// 身份组Id
+        /// <para><em>此属性和UserId只会同时存在一个</em></para>
+        /// </summary>
+        [JsonPropertyName("role_id")]
+        public string? RoleId { get; set; }
         /// <summary>
         /// 用户拥有的子频道权限
         /// <para>
