dd86c1613bcaef8291ee3ae52d2c112234a30d2f
Antecer <antecer@outlook.com>
2022-01-04 14:48:43 +0800

    调整日程对象结构

--- QQChannelBot/Models/Schedule.cs
+++ QQChannelBot/Models/Schedule.cs
@@ -8,12 +8,21 @@ namespace QQChannelBot.Models
     public class Schedule
     {
         /// <summary>
+        /// 构造日程
+        /// </summary>
+        public Schedule() { }
+        /// <summary>
         /// 新建日程
+        /// <para>
+        /// 注1：开始时间必须大于当前时间<br/>
+        /// 注2：结束时间必须大于开始时间<br/>
+        /// 注3：调用API每日创建日程数量有限
+        /// </para>
         /// </summary>
         /// <param name="name">日程名称</param>
         /// <param name="desc">日程描述</param>
-        /// <param name="startTime">开始时间（默认当前时刻）</param>
-        /// <param name="endTime">结束时间（默认持续一天）</param>
+        /// <param name="startTime">开始时间（默认五分钟后）</param>
+        /// <param name="endTime">结束时间（默认持续一小时）</param>
         /// <param name="jumpChannel">日程开始时跳转的频道</param>
         /// <param name="remindType">日程时间即将到达时的提醒方式</param>
         public Schedule(
@@ -26,8 +35,8 @@ namespace QQChannelBot.Models
         {
             Name = name;
             Description = desc;
-            StartTimestamp = new DateTimeOffset(startTime ?? DateTime.Now).ToUnixTimeMilliseconds().ToString();
-            EndTimestamp = new DateTimeOffset(endTime ?? DateTime.Now.AddDays(1)).ToUnixTimeMilliseconds().ToString();
+            StartTimestamp = new DateTimeOffset(startTime ?? DateTime.Now.AddMinutes(5)).ToUnixTimeMilliseconds().ToString();
+            EndTimestamp = new DateTimeOffset(endTime ?? DateTime.Now.AddMinutes(65)).ToUnixTimeMilliseconds().ToString();
             JumpChannelId = jumpChannel?.Id;
             RemindType = remindType;
         }
@@ -35,7 +44,7 @@ namespace QQChannelBot.Models
         /// 日程 id
         /// </summary>
         [JsonPropertyName("id")]
-        public string? Id { get; }
+        public string? Id { get; set; }
         /// <summary>
         /// 日程名称
         /// </summary>
@@ -48,11 +57,13 @@ namespace QQChannelBot.Models
         public string? Description { get; set; }
         /// <summary>
         /// 日程开始时间戳(ms)
+        /// <para>必须大于当前时间</para>
         /// </summary>
         [JsonPropertyName("start_timestamp")]
         public string? StartTimestamp { get; set; }
         /// <summary>
         /// 日程结束时间戳(ms)
+        /// <para>必须大于开始时间</para>
         /// </summary>
         [JsonPropertyName("end_timestamp")]
         public string? EndTimestamp { get; set; }
@@ -60,7 +71,7 @@ namespace QQChannelBot.Models
         /// 创建者
         /// </summary>
         [JsonPropertyName("creator")]
-        public Member? Creator { get; }
+        public Member? Creator { get; set; }
         /// <summary>
         /// 日程开始时跳转到的子频道 id
         /// </summary>
@@ -68,9 +79,19 @@ namespace QQChannelBot.Models
         public string? JumpChannelId { get; set; }
         /// <summary>
         /// 日程提醒类型
+        /// <para>请勿直接读写此属性，而是应该通过RemindType属性读写</para>
         /// </summary>
         [JsonPropertyName("remind_type")]
-        public ScheduleRemindType? RemindType { get; set; }
+        public string Remind { get; set; } = "0";
+        /// <summary>
+        /// 日程提醒类型
+        /// </summary>
+        [JsonIgnore]
+        public ScheduleRemindType? RemindType
+        {
+            get => (ScheduleRemindType)int.Parse(Remind);
+            set => Remind = value?.ToString("D") ?? "0";
+        }
     }
 
     /// <summary>
