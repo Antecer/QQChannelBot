d63557455815e8f76a6a331a0f21109c4d1585c0
Antecer <antecer@outlook.com>
2022-01-18 15:21:06 +0800

    分离鉴权信息结构体声明到独立文件

---
+++ QQChannelBot/Bot/Identity.cs
@@ -0,0 +1,21 @@
+﻿namespace QQChannelBot.Bot
+{
+    /// <summary>
+    /// 鉴权信息
+    /// </summary>
+    public struct Identity
+    {
+        /// <summary>
+        /// 机器人Id
+        /// </summary>
+        public string BotAppId { get; set; }
+        /// <summary>
+        /// 机器人Token
+        /// </summary>
+        public string BotToken { get; set; }
+        /// <summary>
+        /// 机器人密钥
+        /// </summary>
+        public string BotSecret { get; set; }
+    }
+}
